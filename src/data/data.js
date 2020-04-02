c922de31ed5311e3e36e7ea0203af6b1e043ca9a
Freddy <fredduarte@gmail.com>
2020-04-02 20:47:00 +0000

    Final data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,57 +1,192 @@
 const data =
    {
         "EN": {
+            "nav": [
+                {
+                    "name": "Biography",
+                    "to": "/biography"
+                },
+                {
+                    "name": "Albums",
+                    "to": "/albums"
+                },
+                {
+                    "name": "Shows",
+                    "to": "/shows"
+                },
+                {
+                    "name": "Contacts",
+                    "to": "/contacts"
+                }
+            ],
             "bio": {
                 "title": 'Biography',
-                "body": 'tester body'
-
+                "body": 'Palha d\'Aço are a rock band established in April of 1997 in the island of Terceira, Azores. The band evolved from Punk into Thrash Metal and Prog and currently has 2 albums: "Desconcertante", released in April of 2014, and "Sobreviver" released in April of 2016. The band won a local battle of the bands context "Angra Rock" in 2016. The third album is planned to be released in July of 2020.'
             },
-            "media": {
-                "albums": {
-                    "year": '2015',
-                    "month": 'April',
-                    "title": 'Sobreviver',
-                    "body": ''
+            "albums": [
+                {
+                    "date": "April 2014",
+                    "title": "Sobreviver",
+                    "description": "",
+                    "cover": "img/sobreviver.jpg",
+                    "songs": [
+                        'Antro Democrático',
+                        'Destruição',
+                        'Novo Mundo',
+                        'A Tua Vez',
+                        'Estrada do Vício',
+                        'Dança da Noite e do Dia',
+                        'Existência Tangencial',
+                        'Heresia',
+                        'O Medo é Teu',
+                        'Sobreviver'
+                    ],
+                    "links": [
+                        {
+                            "name": "Spotify",
+                            "url": "https://open.spotify.com/album/5a7Gq3bOmWbHuMv9Gvg0nu",
+                            "icon": ["fab", "spotify"]
+                        },
+                        {
+                            "name": "Apple Music",
+                            "icon": ["fab", "apple"],
+                            "url": "https://music.apple.com/pt/album/sobreviver/1154321779"
+                        },
+                    ]
                 },
-                "videos": {
+                {
+                    "date": "April 2012",
+                    "title": "Desconcertante",
+                    "description": "",
+                    "cover": "img/desconcertante.jpg",
+                    "songs": [
+                        'Luta Sem Fim',
+                        'Revolução',
+                        'Sempre Por Ti',
+                        'Legado',
+                        'Abril 97',
+                        'Desconcertante',
+                        'Há Dias',
+                        'Miúda Feia',
+                        'Noite Chata',
+                        'No Liver',
+                        'Em Honra de um Homem',
+                        'Vodka Police',
+                        'Black Beer',
+                        'Joaquim',
 
+                    ],
+                    "links": [
+                    ]
                 },
-                "music": {
-
+            ],
+            "shows": [
+                {
+                    "id": 1,
+                    "date": "2020-07-04",
+                    "title": "Album release 'Nova Desordem'",
+                    "description": "Our new full length album 'Nova Desordem' (New Disorder) will be released at the Centro Cultural in Angra do Heroísmo.",
+                    "location": "Centro Cultural e de Congressos de Angra do Heroísmo",
+                    "photos": {}
                 },
-                "photos": {
-
-                }
-            },
-            "concerts": {
-
-            },
+            ],
             "news": {
 
             }
         },
         "PT": {
-            "bio": {
-                "title": 'Portuguese title',
-                "body": 'Portuguese body'
-            },
-            "media": {
-                "albums": {
-
+            "nav": [
+                {
+                    "name": "Biografia",
+                    "to": "/biography"
                 },
-                "videos": {
-
+                {
+                    "name": "Álbuns",
+                    "to": "/albums"
                 },
-                "music": {
-
+                {
+                    "name": "Concertos",
+                    "to": "/shows"
                 },
-                "photos": {
-
+                {
+                    "name": "Contactos",
+                    "to": "/contacts"
                 }
+            ],
+            "bio": {
+                "title": 'Biografia',
+                "body": 'Os Palha d\'Aço são uma banda de rock constituída em Abril de 1997 na Ilha Terceira, Açores. Inicialmente com sonoridades Punk, a banda evoluiu para um som Metal com influências Thrash e Prog. \n' +
+                    '\n' +
+                    'A banda conta com 2 álbuns "Desconcertante" lançado em Abril de 2014 e "Sobreviver" lançado em Abril de 2016. Foram vencedores da edição 2016 concurso Angra Rock, entretanto já extinto. O terceiro álbum, "Nova Desordem", será lançado em Julho de 2020.'
             },
-            "concerts": {
+            "albums": [
+                {
+                    "date": "Abril 2014",
+                    "title": "Sobreviver",
+                    "description": "",
+                    "cover": "img/sobreviver.jpg",
+                    "songs": [
+                        'Antro Democrático',
+                        'Destruição',
+                        'Novo Mundo',
+                        'A Tua Vez',
+                        'Estrada do Vício',
+                        'Dança da Noite e do Dia',
+                        'Existência Tangencial',
+                        'Heresia',
+                        'O Medo é Teu',
+                        'Sobreviver'
+                    ],
+                    "links": [
+                        {
+                            "name": "Spotify",
+                            "url": "https://open.spotify.com/album/5a7Gq3bOmWbHuMv9Gvg0nu",
+                            "icon": ["fab", "spotify"]
+                        },
+                        {
+                            "name": "Apple Music",
+                            "icon": ["fab", "apple"],
+                            "url": "https://music.apple.com/pt/album/sobreviver/1154321779"
+                        },
+                    ]
+                },
+                {
+                    "date": "Abril 2012",
+                    "title": "Desconcertante",
+                    "description": "",
+                    "cover": "img/desconcertante.jpg",
+                    "songs": [
+                        'Luta Sem Fim',
+                        'Revolução',
+                        'Sempre Por Ti',
+                        'Legado',
+                        'Abril 97',
+                        'Desconcertante',
+                        'Há Dias',
+                        'Miúda Feia',
+                        'Noite Chata',
+                        'No Liver',
+                        'Em Honra de um Homem',
+                        'Vodka Police',
+                        'Black Beer',
+                        'Joaquim',
 
-            },
+                    ],
+                    "links": [
+
+                    ]
+                },
+            ],
+            "shows": [
+                {
+                    "id": 1,
+                    "date": "2020-07-04",
+                    "title": "Apresentação de álbum 'Nova Desordem'",
+                    "description": "O lançamento acontecerá no dia 4 de Julho de 2020 no Centro Cultural de Angra do Heroísmo.",
+                    "location": "Centro Cultural e de Congressos de Angra do Heroísmo",
+                    "photos": {}
+                },
+            ],
             "news": {
 
             }
