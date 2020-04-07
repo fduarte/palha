3eb51ebe59a7f434612271402939ed0bb65b05a0
Freddy <fredduarte@gmail.com>
2020-04-07 16:44:04 +0000

    Updated instagram link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends React.Component {
                        <FontAwesomeIcon icon={['fab', 'bandcamp']} /> <a href="https://palha.bandcamp.com/releases">BandCamp</a>
                        &nbsp;&nbsp;<FontAwesomeIcon icon={['fab', 'youtube']} /> <a href="https://www.youtube.com/channel/UC5wYkK3oB4XCHzapPo-7mrw">YouTube</a> <br />
                        <FontAwesomeIcon icon={['fab', 'facebook']} /> <a href="https://www.facebook.com/palha.rocks">Facebook</a>
-                       &nbsp;&nbsp;<FontAwesomeIcon icon={['fab', 'instagram']} /> <a href="https://www.instagram.com/palha_daco">Instagram</a> <br />
+                       &nbsp;&nbsp;<FontAwesomeIcon icon={['fab', 'instagram']} /> <a href="https://www.instagram.com/palha.rocks">Instagram</a> <br />
                        <FontAwesomeIcon icon={['fas', 'envelope']} /> palha.rocks@gmail.com
                    </p>
                </div>
