d07121757a444e2d9abdcbfb3ad27eeea5446cff
Freddy <fredduarte@gmail.com>
2020-04-23 17:30:24 +0000

    Better promo css

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,12 @@ class Home extends React.Component
                 <h2>Vota em Palha d'Aço no concurso EDP!</h2>
                 <p>
                     <a href="https://edplivebands.edp.com/portugal/banda/palha-daco">
-                        <img style={{width: "250px", float: "left", 'margin-right': "15px"}} src="/img/edp.png" />
+                        <img style={{width: "300px", float: "left", 'margin-right': "15px"}} src="/img/edp.png" />
                     </a>
                     O NOS Alive e o Mad Cool Festival, em Madrid, vão ser os próximos palcos da banda vencedora do EDP Live Bands 2020.
                 </p>
 
-                <p className='text-center mt-5' >
+                <p className='text-center mt-2' >
                     <a href="https://edplivebands.edp.com/portugal/banda/palha-daco">Vota aqui!</a>
                 </p>
             </div>
