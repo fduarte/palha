5a0412b4912b9c31b28e308dbe2583d57d122eb7
Freddy <fredduarte@gmail.com>
2020-06-27 12:29:55 +0000

    Clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component
     render() {
 
         return(
-            <div className="home" style={{textAlign: "center"}}>
+            <div className="home align-content-center text-center">
                 <strong>Lançamento do álbum | AO VIVO NA VITEC</strong>
                 <h2 style={{margin:0, padding:0}}>
                     "Juízo de Valor"
@@ -13,7 +13,7 @@ class Home extends React.Component
                 <strong>-- 4 de Julho 2020 --</strong>
                 <p>
                     <a href="https://www.facebook.com/palha.rocks">
-                        <img className={"img-fluid"} style={{float: "left", width: "550px"}} src="/img/Post_Insta.jpg" />
+                        <img className={"img-fluid"} style={{width: "550px"}} src="/img/Post_Insta.jpg" />
                     </a>
                 </p>
             </div>
