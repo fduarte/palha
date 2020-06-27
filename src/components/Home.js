5d15f2eb00273ad82745d7c9f3093fe66595f0aa
Freddy <fredduarte@gmail.com>
2020-06-27 12:14:56 +0000

    New album

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,16 @@ class Home extends React.Component
     render() {
 
         return(
-            <div className="home">
-                <h2>Vota em Palha d'Aço no concurso EDP!</h2>
+            <div className="home" style={{textAlign: "center"}}>
+                <strong>Lançamento do álbum | AO VIVO NA VITEC</strong>
+                <h2 style={{margin:0, padding:0}}>
+                    "Juízo de Valor"
+                </h2>
+                <strong>-- 4 de Julho 2020 --</strong>
                 <p>
-                    <a href="https://edplivebands.edp.com/portugal/banda/palha-daco">
-                        <img style={{width: "300px", float: "left", 'margin-right': "15px"}} src="/img/edp.png" />
+                    <a href="https://www.facebook.com/palha.rocks">
+                        <img className={"img-fluid"} style={{float: "left", width: "550px"}} src="/img/Post_Insta.jpg" />
                     </a>
-                    O NOS Alive e o Mad Cool Festival, em Madrid, vão ser os próximos palcos da banda vencedora do EDP Live Bands 2020.
-                </p>
-
-                <p className='text-center mt-2' >
-                    <a href="https://edplivebands.edp.com/portugal/banda/palha-daco">Vota aqui!</a>
                 </p>
             </div>
         )
