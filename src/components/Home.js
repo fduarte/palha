1a02f63bb198c984ea3442b48293eefcf11a13b5
Freddy <fredduarte@gmail.com>
2020-06-27 12:32:57 +0000

    Drop a line of text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,12 @@ class Home extends React.Component
 
         return(
             <div className="home align-content-center text-center">
-                <strong>Lançamento do álbum | AO VIVO NA VITEC</strong>
+                <strong>Lançamento do álbum</strong>
                 <h2 style={{margin:0, padding:0}}>
                     "Juízo de Valor"
                 </h2>
                 <strong>-- 4 de Julho 2020 --</strong>
+                <br /> Ao vivo na VITEC
                 <p>
                     <a href="https://www.facebook.com/palha.rocks">
                         <img className={"img-fluid"} style={{width: "550px"}} src="/img/Post_Insta.jpg" />
