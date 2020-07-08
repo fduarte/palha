0ab9de762f426882115830df348329b6951ee0b9
Freddy <fredduarte@gmail.com>
2020-07-08 11:05:56 +0000

    Media section

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component
         return(
             <div className="home align-content-center text-center">
                 <div>
-                    <strong>Lançamento do álbum</strong>
+                    <strong>Novo álbum</strong>
                     <h2 style={{margin:0, padding:0}}>
                         <a href="https://www.youtube.com/watch?v=ZcaI65W0YpA&list=PLKhqkUVfeCKRIYBoq3X4XSuAmXW17DA3e">"Juízo de Valor"</a>
                     </h2>
@@ -20,7 +20,7 @@ class Home extends React.Component
                         </a>
                     </p>
                 </div>
-                <div className="text-left mt-5 ml-3">
+                <div className="text-left mt-2 ml-1">
                     <h2 className="text-center">MEDIA</h2>
                     <ul>
                         <li>
@@ -30,7 +30,7 @@ class Home extends React.Component
                             Passado, Presente, Futuro - <a href="https://pedalameobombo.blogspot.com/2020/07/ppf-3-palha-daco.html">Pedala-me o Bombo</a>
                         </li>
                         <li>
-                            Palha d'Aço lançam single de estreia do novo álbum "Juízo de Valor" - <a href="https://portugarte.pt/2020/06/27/palha-daco-lancam-single-de-estreia-do-novo-album-juizo-de-valor/">Portugarte</a>
+                            Single de estreia do novo álbum "Juízo de Valor" - <a href="https://portugarte.pt/2020/06/27/palha-daco-lancam-single-de-estreia-do-novo-album-juizo-de-valor/">Portugarte</a>
                         </li>
                     </ul>
                 </div>
